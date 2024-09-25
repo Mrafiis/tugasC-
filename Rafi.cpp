62ee28eea8dbf0009a343e875b1c5c12a85a2eba
Mrafiis <mrafiis489@gmail.com>
2024-09-25 09:52:24 +0700

    first commit

---
+++ Rafi.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	
+	char nim[]= "2441031";
+	char nama[]= "Muhammad Rafi IFtikharus Sadat";
+	char jurusan[]= "Teknik Informatika";
+	
+	printf("NIM: %s\n", nim);
+	printf("Nama: %s\n", nama);
+	printf("Jurusan: %s\n", jurusan);
+}
---
+++ Rafi2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int p = 10;
+	int l = 7;
+	int luas = p*l;
+	
+	printf("Luas Persegi Panjang dengan panjang %d cm dan lebar %d cm, adalah %d cm persegi.\n",p,l,luas);
+	return 0;
+}
